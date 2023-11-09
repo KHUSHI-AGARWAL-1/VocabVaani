0cf31f5a84de4d9cd90506273ecd7516a964f19a
KHUSHI-AGRAWAL-1 <khushi.agrawal_cs21@gla.ac.in>
2023-11-09 22:11:29 +0530

    new commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to VocabVaani application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+const db = require("./models");
+const Role = db.role;
+
+const dbConfig = {
+  HOST: "0.0.0.0",
+  PORT: 27017,
+  DB: "VocabVaani"
+};
+
+db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`)
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+
+  
+  async function initial() {
+    try {
+      const count = await Role.estimatedDocumentCount();
+  
+      if (count === 0) {
+        await Role.create([
+          { name: "user" },
+          { name: "moderator" },
+          { name: "admin" }
+        ]);
+  
+        console.log("Roles added to the collection");
+      }
+    } catch (err) {
+      console.error("Error when estimating document count or adding roles", err);
+    }
+  }
